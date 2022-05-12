860dbab4cbd06e60c3c68fb9676831283b3f4996
figo <duttasubhadeep40@gmail.com>
2022-05-12 17:28:37 +0530

    added budget context

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BudgetsProvider } from "./contexts/BudgetsContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<BudgetsProvider>
+			<App />
+		</BudgetsProvider>
+	</React.StrictMode>
 );
